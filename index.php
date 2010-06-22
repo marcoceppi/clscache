6dc38cba0164549ca4f59d6c9ce3832b74043be4
Marco Ceppi <mceppi@danya.com>
2010-06-21 20:24:00 -0400

    Updated documentation and file headers

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Check if your browser's cache is out old.
+ *
+ * A common issue in Web Hosting tech support is how to explain
+ * to customers that they are likely viewing a cached version of
+ * their website and that there is in fact no issue with the
+ * server. This web applications is designed to help eliminate
+ * that issue by providing the user with an easy to use interface
+ * that dispells any Cache issues and furthermore shows the user
+ * how to clear their cache incase it is obsolete.
+ *
+ * @author Marco Ceppi <marco.ceppi@seacrow.org>
+ * @project clscache
+ * @todo everything
+ */
+
+?>
